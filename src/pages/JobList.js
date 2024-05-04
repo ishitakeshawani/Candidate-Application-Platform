0f258f03fdb35a7e6db23dbef085f3b2bfd5757e
Ishita <ishita.k@turvo.com>
2024-05-04 14:59:06 +0530

    Used useEffect and useJobs to getJobs data

--- src/pages/JobList.js
+++ src/pages/JobList.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { getJobs, useJobs } from '../features/jobSlice'
+import { useDispatch } from "react-redux";
 
 export default function JobList() {
+    const dispatch = useDispatch();
+    // fetch jobs from API using getJobs
+    useEffect(() => {
+      dispatch(getJobs());
+    }, [dispatch]);
+    const payload = {
+        "limit":20,
+        "offset":21
+    }
+    // get jobs, isLoading, numberOfJobs by Destructuring using useJobs hook
+    const { jobs, isLoading, numberOfJobs } = useJobs(payload);
+    console.log(jobs, isLoading, numberOfJobs);
     
   return (
     <div>JobList</div>
