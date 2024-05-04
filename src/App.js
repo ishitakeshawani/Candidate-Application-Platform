1be325726a8a183f1f12d422af0a17cd76dc904e
Ishita <ishita.k@turvo.com>
2024-05-04 14:01:52 +0530

    Created JobList page and used it in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Box from '@mui/material/Box';
+import JobList from './pages/JobList';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100dvw",
+        padding: "1rem",
+        gap: '5rem'
+      }}
+    >
+      <h3>Search jobs</h3>
+      <JobList />
+    </Box>
   );
 }
 
